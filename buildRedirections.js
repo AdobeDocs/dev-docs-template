fcb1cfd93b42d458e2fba2a76c0c4fa174036a1d
Melissa Garcia <ens15038@adobe.com>
2025-03-19 11:21:46 -0700

    update scripts

--- buildRedirections.js
+++ buildRedirections.js
@@ -11,21 +11,31 @@ try {
     let results = globSync(__dirname + '/src/pages/**/*.md');
     let data = [];
 
-    // Fixes paths that end in a trailing slash that shouldn't
-    results.forEach((mdFilePath) => {
-        let originalFilePath = mdFilePath;
+    results.forEach(mdFilePath => {
         mdFilePath = mdFilePath.replace(__dirname + '/src/pages', pathPrefix);
         mdFilePath = path.resolve(mdFilePath);
 
+        // Fixes paths that don't end in a trailing slash but should.
+        // index.md is a directory-level URL that needs a trailing slash
+        if(mdFilePath.endsWith('index.md')) {
+            const source = mdFilePath.replace('/index.md', '');
+            data.push({
+                "Source" : source,
+                "Destination" : source + '/'
+            });
+            data.push({
+                "Source" : source + '/index',
+                "Destination" : source + '/'
+            });
+        }
+        // Fixes paths that end in a trailing slash but shouldn't.
         // skip any index.md or config.md as they don't need redirect
-        if(!mdFilePath.includes('index.md')) {
-            if(!mdFilePath.includes('config.md')) {
-                mdFilePath = mdFilePath.replace('.md', '/');
-                data.push({
-                    "Source" : mdFilePath,
-                    "Destination" : mdFilePath.replace(/\/$/, "")
-                });
-            }
+        else if(!mdFilePath.endsWith('config.md')) {
+            const source = mdFilePath.replace('.md', '/');
+            data.push({
+                "Source" : source,
+                "Destination" : source.replace(/\/$/, "")
+            });
         }
     });
 
